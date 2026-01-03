b6e638f4c2b93c8677d0b72de8620da0f0dd64a0
kyra-37 <adarieaswarkarthik375@gmail.com>
2026-01-03 22:34:26 +0530

    03J2K26.py

--- 01J2K26.py
+++ 01J2K26.py
@@ -1,3 +1,4 @@
 print("hello karthik")
 print("2k26 is all yours make this day count")
 print("anaconda")
+print("at the end of the day you are all alone but not lonely , remember karthik you always have you ")
